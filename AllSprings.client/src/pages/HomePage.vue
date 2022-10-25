e7e61d986c3a21c01f92693754d288477ec1b70c
Kade Fullmer <fullmer24@gmail.com>
2022-10-25 10:03:01 -0600

    ad

--- AllSprings.client/src/pages/HomePage.vue
+++ AllSprings.client/src/pages/HomePage.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+  <!-- <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="home-card p-5 bg-white rounded elevation-3">
       <img src="" alt="" class="rounded-circle">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
         Vue 3 Starter
       </h1>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
